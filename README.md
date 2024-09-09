c6797269f0fe4c52ca2f1600418975c0f01dff17
phamiltonzeiss <paul.hamilton@zeiss.com>
2024-09-09 08:52:07 -0700

    Initial Collaboration Commit
    Several modifications based on beta testing, including:
      * Re-organizing section flow to better match call priorities
      * Navigation links in each section
      * Added Service Admin PM Request email template to Preventative Maintenance navigation section (and made it a section)
      * Added Teamwork Bonus Lead link to EOS section

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SNAP
+
+## Script and Note Automation Process
+
+SNAP is intended to aid Zeiss Technical Support Engineers in managing their calls and generating CRM-friendly information blobs for copy/pasting into the ticket creation form.
+
+## Contact
+
+Author: Divon Pleasant
+Email: divon.pleasant@zeiss.com
---
+++ assets/js/serviceadminpmreqemail.js
@@ -0,0 +1,37 @@
+function serviceadminpmreqemail(event) {
+    // Prevent the default action (which is following the link)
+    event.preventDefault();
+
+    // Retrieve form values
+    var serial = document.getElementById('serial').value;
+	var account = document.getElementById('account').value;
+	var instrumentAddress = document.getElementById('instrument-address').value;
+    var localContactPerson = document.getElementById('local-contact-person').value;
+	var localContactPhone = document.getElementById('phone').value;
+
+    // Construct the subject line and body of the email
+    var subject = "Preventative Maintenance Request";
+    var body = "Hi Admin Team,\n\n" +
+			   "Customer has called in to request a contract PM with the following info:\n\n" +
+			   "Customer Name: " + account + "\n\n" +
+			   "Site Location: " + instrumentAddress + "\n\n" +
+			   "Contact Name/Phone Number: " + localContactPerson + " / " + localContactPhone + "\n\n" +
+			   "Serial Number: " + serial + "\n\n" +
+			   "Contract #: \n\n" +
+			   "Date PM Only Requested: \n\n";
+
+    // Encode the subject and body
+    subject = encodeURIComponent(subject);
+    body = encodeURIComponent(body);
+
+    // Construct the mailto link
+    var mailtoLink = "mailto:serviceoperationsadmin.med.us@zeiss.com" +
+        "?subject=" + subject +
+        "&body=" + body;
+
+    // Open the mailto link
+    window.location.href = mailtoLink;
+}
+
+// Add event listener to the link
+document.getElementById('serviceadminpmreqemailLink').addEventListener('click', serviceadminpmreqemail);
