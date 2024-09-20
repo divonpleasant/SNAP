a8da50af0e267b8506b55509f9e9533cfaf0bc70
Divon Pleasant <78009070+divonpleasant@users.noreply.github.com>
2024-09-19 20:22:14 -0700

    Add files via upload

--- assets/js/form-background.js
+++ assets/js/form-background.js
@@ -2,7 +2,7 @@ document.querySelectorAll('input[type="text"], textarea').forEach(function (inpu
     if (input.id !== "problem-description") {
         input.addEventListener('input', function () {
             if (this.value) {
-                this.style.backgroundColor = '#8f8f8f';
+                this.style.backgroundColor = '#e6f2ff';
             } else {
                 this.style.backgroundColor = ''; // Reset background color
             }
