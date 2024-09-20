04def0b25ea1d2cd087db3e776f5c57319dfbde8
Divon Pleasant <78009070+divonpleasant@users.noreply.github.com>
2024-09-19 18:18:13 -0700

    Add files via upload

--- assets/js/printintnotes.js
+++ assets/js/printintnotes.js
@@ -5,21 +5,16 @@ document.getElementById('printintnotes').addEventListener('click', function(even
 
     var fields = [
         { label: "Time Spent", value: document.getElementById('remote-time').value },
-        { label: "Device Module", value: document.getElementById('device-module').value },
-        { label: "Sub-Module", value: document.getElementById('sub-module').value },
         { label: "Error Code Group", value: document.getElementById('error-group').value },
         { label: "Error Code", value: document.getElementById('error-code').value },
         { label: "Action Code", value: document.getElementById('action-code').value },
-        { label: "Cirrus HD-OCT/Review Workstation Status", value: document.getElementById('work-station-status').value },
-        { label: "OS (C:)", value: document.getElementById('c-drive').value },
-        { label: "Data (E:)", value: document.getElementById('e-drive').value },
         { label: "Other Internal Notes", value: document.getElementById('other-internal-notes').value }
     ];
 
     var data = `
 Time Spent: ${document.getElementById('remote-time').value}
-Device Module: ${document.getElementById('device-module').value}
-Sub Module: ${document.getElementById('sub-module').value}
+Device Module: Network/Connectivity
+Sub Module: N/A*
 Error Code Group: ${document.getElementById('error-group').value}
 Error Code: ${document.getElementById('error-code').value}
 Action Code: ${document.getElementById('action-code').value}
@@ -30,13 +25,7 @@ Mobile/Office Phone Number: ${document.getElementById('phone1').value}
 Email Address: ${document.getElementById('email1').value}
 ${document.getElementById('other-internal-notes').value}
 
-FSE Search: 
-Primary FSE: ${document.getElementById('primary-fse').value}
-Field Service Supervisor: ${document.getElementById('field-service-supervisor').value}
 Was Remote Support Provided? ${document.getElementById('remote-support').checked ? "Yes" : "No"}
-Cirrus HD-OCT/Review Workstation Status: ${document.getElementById('work-station-status').value}
-OS (C:): ${document.getElementById('c-drive').value}
-Data (E:): ${document.getElementById('e-drive').value}
 `;
 
     // Remove empty lines
