708ffd15ccd35ace18e6e885895dd036f20ae19b
phamiltonzeiss <paul.hamilton@zeiss.com>
2024-10-11 11:44:39 -0700

    Fix syncFields Error
    Removal of the device-serial-number duplicate field in the HTML caused the syncfields script to throw an error into the console. It appears pretty benign, not causing any other functionality to fail, but this fixes the issue. I do wonder if we need the dupe field on description/problem-description though if it's the only one, although the gray field highlighting makes it clear why it appears in more than one place (as it's included in the Request section and RAFTA notes).

--- assets/js/syncFields.js
+++ assets/js/syncFields.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // New event listeners for serial and description
-    var serial = document.getElementById("serial");
-    var deviceSerialNumber = document.getElementById("device-serial-number");
+    // New event listener description
     var description = document.getElementById("description");
     var problemDescription = document.getElementById("problem-description");
-
-    serial.addEventListener("input", function() {
-        deviceSerialNumber.value = serial.value;
-    });
 
     description.addEventListener("input", function() {
         problemDescription.value = description.value;
