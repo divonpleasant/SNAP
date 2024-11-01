a99e3631bece32fc8d2996340fedab5794e4dd4f
Divon Pleasant <78009070+divonpleasant@users.noreply.github.com>
2024-11-01 13:37:26 -0700

    Add files via upload

--- assets/js/serviceadminpmreqemail.js
+++ assets/js/serviceadminpmreqemail.js
@@ -1,5 +1,9 @@
-function serviceadminpmreqemail(event) {
-    // Prevent the default action (which is following the link)
+function showOverlay(event) {
+    event.preventDefault();
+    document.getElementById('overlay-pmreq').style.display = 'flex';
+}
+
+function proceedToSendEmail(event) {
     event.preventDefault();
 
     // Retrieve form values
@@ -7,18 +11,21 @@ function serviceadminpmreqemail(event) {
 	var account = document.getElementById('account').value;
 	var instrumentAddress = document.getElementById('instrument-address').value;
     var localContactPerson = document.getElementById('local-contact-person').value;
-	var localContactPhone = document.getElementById('phone').value;
+    var localContactPhone = document.getElementById('phone').value;
+    var contract = document.getElementById('contract-number').value.trim();
+    var currentDate = new Date().toDateString();
 
     // Construct the subject line and body of the email
-    var subject = "Preventative Maintenance Request";
+    var subject = "Preventative Maintenance Request S/N: " + serial;
     var body = "Hi Admin Team,\n\n" +
 			   "Customer has called in to request a contract PM with the following info:\n\n" +
 			   "Customer Name: " + account + "\n\n" +
 			   "Site Location: " + instrumentAddress + "\n\n" +
 			   "Contact Name/Phone Number: " + localContactPerson + " / " + localContactPhone + "\n\n" +
-			   "Serial Number: " + serial + "\n\n" +
-			   "Contract #: \n\n" +
-			   "Date PM Requested: \n\n";
+               "Serial Number: " + serial + "\n\n" +
+               "Contract #: " + contract + "\n\n" +
+               "Date PM Requested: " + currentDate + "\n\n" +
+               "Regards,";
 
     // Encode the subject and body
     subject = encodeURIComponent(subject);
@@ -31,7 +38,19 @@ function serviceadminpmreqemail(event) {
 
     // Open the mailto link
     window.location.href = mailtoLink;
+
+    // Hide the overlay after proceeding
+    document.getElementById('overlay-pmreq').style.display = 'none';
 }
 
-// Add event listener to the link
-document.getElementById('serviceadminpmreqemailLink').addEventListener('click', serviceadminpmreqemail);
+// Add event listener to the fseupdateLink for showing the overlay
+document.getElementById('serviceadminpmreqemailLink').addEventListener('click', showOverlay);
+
+// Add event listener to the "Proceed" button for sending the email
+document.getElementById('pm-proceed-button').addEventListener('click', proceedToSendEmail);
+
+// Close button functionality for the overlay
+document.getElementById('close-overlay-pmreq').addEventListener('click', function () {
+    document.getElementById('overlay-pmreq').style.display = 'none';
+
+});
