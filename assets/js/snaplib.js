1d0f85364d59cc4932ac9cf3368e8c08adef6c7d
phamiltonzeiss <paul.hamilton@zeiss.com>
2024-12-03 08:44:04 -0800

    Reset Settings
    Sets setting values to appropriate defaults.

--- assets/js/snaplib.js
+++ assets/js/snaplib.js
@@ -6,12 +6,12 @@ var curr_date = new Date();
 var utc_year = curr_date.getUTCFullYear();
 // Setting Defaults
 var debug_mode = true;
-var debug_level = 5; // Range of 0 (same as debug_mode = false) to 5 (all debug messages)
+var debug_level = 4; // Range of 0 (same as debug_mode = false) to 5 (all debug messages)
 var copy_alert = false;
 var xc_alert = true;
 var copy_descr = true;
 var con_clear = true;
-var dark_mode = true;
+var dark_mode = false;
 var sign_email = false;
 var tag = '';
 var style_sheet = 'main';
